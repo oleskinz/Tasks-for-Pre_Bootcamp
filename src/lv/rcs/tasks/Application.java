55c4e5a46cd7dc0b65cc7cecf49fb1331d6f98bb
oleskinz <olesjashandiba@gmail.com>
2019-08-08 23:22:13 +0300

    add PrimeGenerator

--- src/lv/rcs/tasks/Application.java
+++ src/lv/rcs/tasks/Application.java
@@ -2,7 +2,8 @@ package lv.rcs.tasks;
 
 public class Application {
     public static void main(String[] args) {
-        Task05.run();
+        PrimeGenerator.run();
+        //Task05.run();
     }
 
 }
